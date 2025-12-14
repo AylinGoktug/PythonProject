352d64baea2004239c1eb1c59e1bd27e5a33e809
AylinGoktug <aylingoktug@gmail.com>
2025-12-14 18:50:21 +0300

    _01_print_comment

---
+++ python_pure_tutorials/_01_print_comment.py
@@ -0,0 +1,21 @@
+# Single Comment
+
+
+"""
+Multiple Comment
+"""
+
+#===================================================================
+#                              PRINT
+#===================================================================
+
+print("Python Öğreniyorum")
+
+print("Python","Java","Nodejs")
+
+print("Python" "Java" "Nodejs")
+
+print(1453)
+print(14.53)
+print(14,53)
+
