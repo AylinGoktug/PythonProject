2656864644537f7c5c257915583ee2e6f6264c84
AylinGoktug <aylingoktug@gmail.com>
2025-12-20 17:00:10 +0300

    Operators

--- python_pure_tutorials/_04_Operators.py
+++ python_pure_tutorials/_04_Operators.py
@@ -8,8 +8,24 @@ number2=2
 print("Topla : ", (number1 + number2))
 print("Çıkar : ", (number1 - number2))
 print("Çarp : ", (number1 * number2))
-print("Böl : ", (number1 / number2))    # Bölüm : Virgüllü kalan
-print("Bölüm : ", (number1 // number2))     # Bölüm : Tam sayılı kalan
+print("Böl : ", (number1 / number2))    # Bölüm : Virgüllü bölüm
+print("Bölüm : ", (number1 // number2))     # Bölüm : Tam sayılı bölüm
 print("Kalan : ", (number1 % number2))      # mod alma = Kalan
 print("Üslü : ", (number1 ** number2))  # üs alma ** ile çalışır
+
+
+#======================================================
+#       Logic İşlemler
+#======================================================
+
+print("Eşit mi ", number1==number2)
+print("Eşit değil mi ", number1!=number2)
+print("Büyük mü ", number1>number2)
+print("Büyük Eşit mi ", number1>=number2)
+print("Küçük mü ", number1<number2)
+print("Küçük Eşit mi ", number1<=number2)
+
+# && = VE
+# || = VEYA
+# ! = DEĞİL
 
