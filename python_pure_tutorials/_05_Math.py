4d7421c5a15e1cdaf523a225a6fa2d562faf6a9d
AylinGoktug <aylingoktug@gmail.com>
2025-12-20 20:13:03 +0300

    Math, Logic

---
+++ python_pure_tutorials/_05_Math.py
@@ -0,0 +1,38 @@
+#======================================================
+#       MATH
+#======================================================
+import math
+print("PI",math.pi)
+
+#======================================================
+#       SABİT
+#======================================================
+
+print("PI",math.pi)
+print("E",math.e)
+
+
+#======================================================
+#       DÖRT İŞLEM
+#======================================================
+
+print("Karekök : ", math.sqrt(16))
+print("Üslü Sayı", math.pow(2,5))
+print("Faktöriyel : ", math.factorial(5))
+print("Yukarı Yuvarla : ", math.ceil(4.001))
+print("Aşağı Yuvarla : ", math.floor(4.999))
+
+
+#======================================================
+#       LOGARİTMA
+#======================================================
+
+print("Log : ", math.log(100))
+
+number_list=[1,2,3,4,5]
+print("En Büyük : ", max(number_list))
+print("En Küçük : ", min(number_list))
+print("Toplam : ", sum(number_list))
+print("Ortalama : ", sum(number_list) / len(number_list))
+
+
