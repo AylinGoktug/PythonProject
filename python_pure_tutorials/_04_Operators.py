bd89371be1e2cb9b06293328e00974bb88c9c627
AylinGoktug <aylingoktug@gmail.com>
2025-12-20 16:44:19 +0300

    Operators

---
+++ python_pure_tutorials/_04_Operators.py
@@ -0,0 +1,15 @@
+#======================================================
+#       Sayısal İşlemler (Dört İşlem)
+#======================================================
+
+
+number1=13
+number2=2
+print("Topla : ", (number1 + number2))
+print("Çıkar : ", (number1 - number2))
+print("Çarp : ", (number1 * number2))
+print("Böl : ", (number1 / number2))    # Bölüm : Virgüllü kalan
+print("Bölüm : ", (number1 // number2))     # Bölüm : Tam sayılı kalan
+print("Kalan : ", (number1 % number2))      # mod alma = Kalan
+print("Üslü : ", (number1 ** number2))  # üs alma ** ile çalışır
+
