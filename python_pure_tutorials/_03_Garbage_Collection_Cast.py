c94db9d71454252b3d3cf860acb24049cde0a2de
AylinGoktug <aylingoktug@gmail.com>
2025-12-20 16:31:19 +0300

    Garbage_Collection_Cast

---
+++ python_pure_tutorials/_03_Garbage_Collection_Cast.py
@@ -0,0 +1,28 @@
+# Çöp data
+
+number1=44
+number1=13
+print(number1)
+
+
+# None Type Data
+
+data = None
+print("Boş Data : ", data)
+print(type(data))
+
+
+# Cast :
+
+number2="44"
+print(type(number2))
+#print(number2 + 16)
+#string + integer hata verir
+print(int(number2) + 16)
+print(float(number2) + 16)
+
+print("C:\\User\\AppData")
+print(r"C:\\User\\AppData")
+print(r"C:\User\AppData")
+# \r : Escape katakterdir. Karakteri devre dışı bırakmak için kullanılır.
+
